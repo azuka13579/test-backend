dc40437dc091c1e6cca2efa9d9d67e2f8d32cd95
Azka <zkrdty@gmail.com>
2026-01-27 22:28:07 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,39 +25,39 @@ class DatabaseSeeder extends Seeder
             'role' => 'creator',
         ]);
 
-        // // Buat 5 User Viewer tambahan secara random
-        // for ($i = 1; $i <= 5; $i++) {
-        //     User::create([
-        //         'username' => 'viewer_' . $i,
-        //         'email' => "viewer$i@test.com",
-        //         'password' => Hash::make('password'),
-        //         'role' => 'viewer',
-        //     ]);
-        // }
+        // Buat 5 User Viewer tambahan secara random
+        for ($i = 1; $i <= 5; $i++) {
+            User::create([
+                'username' => 'viewer_' . $i,
+                'email' => "viewer$i@test.com",
+                'password' => Hash::make('password'),
+                'role' => 'viewer',
+            ]);
+        }
 
-        // $allUsers = User::all();
-        // $faker = \Faker\Factory::create();
+        $allUsers = User::all();
+        $faker = \Faker\Factory::create();
 
-        // // 2. BUAT 30 POST
-        // for ($i = 1; $i <= 30; $i++) {
-        //     $post = Post::create([
-        //         'user_id' => $creator->id, // Semua post dibuat oleh budi_creator
-        //         'title' => $faker->sentence(6),
-        //         'content' => $faker->paragraphs(3, true),
-        //         'image_url' => 'https://picsum.photos/seed/' . Str::random(5) . '/800/600',
-        //     ]);
+        // 2. BUAT 30 POST
+        for ($i = 1; $i <= 30; $i++) {
+            $post = Post::create([
+                'user_id' => $creator->id, // Semua post dibuat oleh budi_creator
+                'title' => $faker->sentence(6),
+                'content' => $faker->paragraphs(3, true),
+                'image_url' => 'https://picsum.photos/seed/' . Str::random(5) . '/800/600',
+            ]);
 
-        //     // 3. BUAT KOMENTAR UNTUK SETIAP POST
-        //     // Setiap post akan dapet 2 - 4 komen dari user random
-        //     $jumlahKomen = rand(2, 4);
-        //     for ($j = 1; $j <= $jumlahKomen; $j++) {
-        //         Comment::create([
-        //             'post_id' => $post->id,
-        //             'user_id' => $allUsers->random()->id, // Komen dari user acak (bisa creator/viewer)
-        //             'content' => $faker->sentence(),
-        //         ]);
-        //     }
-        // }
+            // 3. BUAT KOMENTAR UNTUK SETIAP POST
+            // Setiap post akan dapet 2 - 4 komen dari user random
+            $jumlahKomen = rand(2, 4);
+            for ($j = 1; $j <= $jumlahKomen; $j++) {
+                Comment::create([
+                    'post_id' => $post->id,
+                    'user_id' => $allUsers->random()->id, // Komen dari user acak (bisa creator/viewer)
+                    'content' => $faker->sentence(),
+                ]);
+            }
+        }
 
         $this->command->info('Seeding selesai! 1 Creator, 5 Viewer, 30 Post, dan ratusan komen berhasil dibuat.');
     }
